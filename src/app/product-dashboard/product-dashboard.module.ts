eea845c8f32b2b6aed64cec8e803bafebeb76f33
SindhuSanga <sindhu.choudary7gmail.com>
2019-04-12 23:14:05 +0530

    dashboard

--- src/app/product-dashboard/product-dashboard.module.ts
+++ src/app/product-dashboard/product-dashboard.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductDashboardComponent } from './product-dashboard/product-dashboard.component';
+import { ProductDashboardRoutingModule} from './product-dashboard-routing.module'
 
 @NgModule({
   declarations: [ProductDashboardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ProductDashboardRoutingModule
+  ],
+  exports: [
+    ProductDashboardComponent
   ]
 })
 export class ProductDashboardModule { }
