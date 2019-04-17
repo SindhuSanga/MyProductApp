9d6310e78efa90238ee2a3df453be845440d45c0
SindhuSanga <sindhu.choudary7gmail.com>
2019-04-17 19:00:21 +0530

    products added

--- src/app/category-list/category-list.component.ts
+++ src/app/category-list/category-list.component.ts
@@ -1,24 +1,34 @@
 import { Component, OnInit } from '@angular/core';
-import {Product } from '../product';
-import {PRODUCTS} from '../mock-products';
+// import {Product } from '../product';
+// import {PRODUCTS} from '../mock-products';
 
+import {Config,ConfigService} from '../config/config.service';
 @Component({
   selector: 'app-category-list',
   templateUrl: './category-list.component.html',
+  providers: [ ConfigService ],
   styleUrls: ['./category-list.component.css']
 })
 export class CategoryListComponent implements OnInit {
 
-  Product:Product[];
-  prodList=PRODUCTS;
+  // Product:Product[];
+ prodList:Config;
 
- 
-  constructor() { }
+  error: any;
+  headers: string[];
+  config: Config;
+  constructor(private configService:ConfigService) { }
 
 
 
   ngOnInit() {
-          
+   this.showConfigResponse();       
   }
-
+  showConfigResponse() {
+    this.configService.getConfigResponse()
+      .subscribe(resp => {
+        console.log(resp)
+        this.prodList=resp.body;
+      })
+    }
 }
